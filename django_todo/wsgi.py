4f332954a2a92d1b143ae2b310f828e209aa3caa
Wini996 <libbywinfield101@gmail.com>
2020-02-19 09:56:49 +0000

    edit to wsgi

--- django_todo/wsgi.py
+++ django_todo/wsgi.py
@@ -13,4 +13,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_todo.settings")
 
-application = django_todo_wsgi_application()
+application = get_wsgi_application()
