4f9328cfbed04f3ca94961beb37bb33f25f26294
Wini996 <libbywinfield101@gmail.com>
2020-02-19 15:30:01 +0000

    added new secret key

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '__-iviqah0#)k&%@r^_8uc)ggc4fk-ld5w6(l)%-9cwbr*=r(d'
+SECRET_KEY = 'Secret'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
